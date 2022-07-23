0ad94241305eadbd0753b7c4d5de9e5791f81de9
Neo-Machina <andreistatache1997@gmail.com>
2022-07-23 22:26:07 +0200

    snack4 completed

--- snack4/index.php
+++ snack4/index.php
@@ -1,2 +1,25 @@
 <!-- Prendere un paragrafo abbastanza lungo, contenente diverse frasi. 
 Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack4</title>
+</head>
+<body>
+    <?php 
+        $longParagraph = 'Lorem ipsum. Dolor sit amet consectetur adipisicing elit. Fugiat tempora dolores. Voluptatem laudantium aut temporibus magni sed adipisci esse quo. Ducimus quaerat eaque maiores at blanditiis voluptates ex dignissimos a.';
+        $paragraphs = explode('.', $longParagraph);
+    ?>
+
+    <?php for($i = 0; $i < count($paragraphs); $i++) {?>
+        <?php $singleParagraph = $paragraphs[$i] ?>
+
+        <p> <?php echo $singleParagraph ?> </p>
+    <?php }?>
+    
+</body>
+</html>
